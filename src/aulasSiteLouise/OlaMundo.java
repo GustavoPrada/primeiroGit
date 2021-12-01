774899ac083ee278b94e577d8eb06bbcdd8eae7e
GustavoPrada <gustavoluiz@gmail.com>
2021-11-30 22:45:27 -0300

    first comit

---
+++ src/aulasSiteLouise/OlaMundo.java
@@ -0,0 +1,14 @@
+package aulasSiteLouise;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String tema = "Tricolor";
+		
+		System.out.println("Ola Mundo " + tema + "!!");
+		
+	}
+
+}
